9c8ed01e1162e14a21368a386dbcc637050a2f0e
Csaba Puskás <36250394+csabi333@users.noreply.github.com>
2020-10-04 19:09:08 +0200

    finished mobile view, added smooth scrolling

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+
 $(document).ready(function(){
     $('.menu-toggler').on('click',function(){
         $('.menu-toggler').toggleClass('open');
         $('.top-nav').toggleClass('open');
-    })
+    });
+
+    $('.top-nav .nav-link').on('click',function(){
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    });
+
+    $('nav a[href*="#"]').on('click',function(){
+        $('html, body').animate({
+            scrollTop:$($(this).attr('href')).offset().top-100
+        },2000);
+    });
 });
